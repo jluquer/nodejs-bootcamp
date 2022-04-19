517ec2d26860baf3863275b41997771a0c41c40e
jluquer <javier.lr96@gmail.com>
2022-04-19 16:28:39 +0200

    vardump

--- uptask/index.js
+++ uptask/index.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const db = require("./config/db");
 const app = express();
 const port = process.env.PORT || 3000;
+const helpers = require("./helpers");
 
 // DB
 require("./models/Proyecto");
@@ -22,8 +23,14 @@ app.set("views", path.join(__dirname, "./views"));
 app.use(express.static("public"));
 
 // json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Var dump
+app.use((req, res, next) => {
+  res.locals.vardump = helpers.vardump;
+  next();
+});
 
 // Routes
 app.use("/", routes());
